36a048027c1166de8c36bbc83f9e7f2931f1784b
stinki <60400050+xtrathicc@users.noreply.github.com>
2024-08-21 10:42:28 +0200

    internal fix

--- GeneralFunctions.lua
+++ GeneralFunctions.lua
@@ -1,17 +1,15 @@
 ChatPrefix = "@VintageEclipse:"
 
-function Dump(o)
+function dump(o)
   if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then
-        k = '"' .. k .. '"'
-      end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-    end
-    return s .. '} '
+     local s = '{ '
+     for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. dump(v) .. ','
+     end
+     return s .. '} '
   else
-    return tostring(o)
+     return tostring(o)
   end
 end
 
